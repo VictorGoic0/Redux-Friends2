8686a2f729d81f4fcf54305ffe8e2f2bf9666b63
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-03-07 17:29:46 -0500

    Redux logger boilerplate added. Login form state, changeHandler, and inputs added.

--- friends/src/index.js
+++ friends/src/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 
 import reducer from './reducers';
 
-const store = createStore(reducer, applyMiddleware(thunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk, logger)));
 
 ReactDOM.render(<Provider store={store}>
   <App />
